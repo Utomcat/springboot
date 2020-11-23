2410d9c2a9b0e65f5109bcc71de89f624af83f40
ranyk <1390851149@qq.com>
2020-11-23 14:29:54 +0800

    更新至 Spring Boot 编程思想 第五章 理解自动装配

--- thinking-in-spring-boot-samples/first-app-by-api/src/main/java/thinkinginspringboot/firstappbyapi/FirstAppByApiApplication.java
+++ thinking-in-spring-boot-samples/first-app-by-api/src/main/java/thinkinginspringboot/firstappbyapi/FirstAppByApiApplication.java
@@ -1,15 +1,32 @@
 package thinkinginspringboot.firstappbyapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 /**
  * @author ranyk
  */
+@Slf4j
 @RestController
-@SpringBootApplication
+//@SpringBootApplication //same as @Configuration @ComponentScan @EnableAutoConfiguration
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class FirstAppByApiApplication  {
 
     public static void main(String[] args) {
@@ -24,6 +41,23 @@ public class FirstAppByApiApplication  {
     @GetMapping("/index")
     public String index(){
         return "hello world!";
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> hellWorld(){
+        return route(GET("/hello-world"),request -> ok().body(Mono.just("Hello,World!"),String.class));
+    }
+
+    /*@Bean
+    public ApplicationRunner runner(WebServerApplicationContext context){
+        return args -> {
+            log.error("当前 WebServer 实现类为: " + context.getWebServer().getClass().getName());
+        };
+    }*/
+
+    @EventListener(WebServerInitializedEvent.class)
+    public void onWebServerReady(WebServerInitializedEvent event){
+        log.error("当前 WebServer 实现类为: " + event.getWebServer().getClass().getName());
     }
 
 
