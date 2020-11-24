80d17de270fb9685de657ebbe1546797260218f9
ranyk <1390851149@qq.com>
2020-11-24 23:26:43 +0800

    完成 Spring Boot 编程思想 第五章 5.5 节的笔记

--- thinking-in-spring-boot-samples/first-app-by-gui/src/main/java/thinking/in/spring/boot/config/WebConfiguration.java
+++ thinking-in-spring-boot-samples/first-app-by-gui/src/main/java/thinking/in/spring/boot/config/WebConfiguration.java
@@ -1,6 +1,8 @@
 package thinking.in.spring.boot.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.context.WebServerInitializedEvent;
 import org.springframework.context.annotation.Bean;
@@ -24,7 +26,15 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 @Slf4j
 //@Configuration
 @SpringBootApplication
+//@EnableAutoConfiguration
+// 使用 @EnableAutoConfiguration 注解和使用 @SpringBootApplication 注解的不同之处在于
+// @EnableAutoConfiguration 没有 EnhancerBySpringCGLIB
+// @SpringBootApplication  有 SpringBootApplication
 public class WebConfiguration {
+
+
+    public String a;
+
 
     @Bean
     public RouterFunction<ServerResponse> hellWorld(){
@@ -37,6 +47,14 @@ public class WebConfiguration {
             log.error("当前 WebServer 实现类为: " + context.getWebServer().getClass().getName());
         };
     }*/
+
+    @Bean
+    public ApplicationRunner runner(BeanFactory beanFactory){
+        return args -> {
+            log.error("当前 helloWorld Bean 实现类为: " + beanFactory.getBean("hellWorld").getClass().getName());
+            log.error("当前 WebConfiguration Bean 实现类为: " + beanFactory.getBean(WebConfiguration.class).getClass().getName());
+        };
+    }
 
     @EventListener(WebServerInitializedEvent.class)
     public void onWebServerReady(WebServerInitializedEvent event){
